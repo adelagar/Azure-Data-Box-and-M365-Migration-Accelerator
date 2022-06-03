ff1c7139d9f1f660170afa8ddba8607280ad3b5a
Jason Masten <jamasten@microsoft.com>
2022-06-02 21:19:49 -0400

    fixed formatting

--- scripts/Add-AzureFileShare.ps1
+++ scripts/Add-AzureFileShare.ps1
@@ -24,7 +24,8 @@ param(
 $Password = ConvertTo-SecureString -String $VMPassword -AsPlainText -Force
 [pscredential]$Credential = New-Object System.Management.Automation.PSCredential ($VMUsername, $Password)
 
-Invoke-Command -Credential $Credential -ScriptBlock {
+Invoke-Command -Credential $Credential -ScriptBlock 
+{
     # Save the password so the drive will persist on reboot
     cmd.exe /C "cmdkey /add:`"$Using:StorageAccountName.file.$Using:StorageSuffix`" /user:`"localhost\$Using:StorageAccountName`" /pass:`"$Using:StorageKey`""
         
