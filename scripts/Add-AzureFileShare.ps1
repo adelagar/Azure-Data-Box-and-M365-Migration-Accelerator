19e43df8aeb3a7b44c79f294e65bb4c817a3c34a
Jason Masten <jamasten@microsoft.com>
2022-06-03 19:25:12 -0400

    Changed share mount to login script

--- scripts/Add-AzureFileShare.ps1
+++ scripts/Add-AzureFileShare.ps1
@@ -10,52 +10,14 @@ param(
     [String]$StorageKey,
     
     [Parameter(Mandatory)]
-    [String]$StorageSuffix,
-    
-    [Parameter(Mandatory)]
-    [String]$VMUsername,
-    
-    [Parameter(Mandatory)]
-    [String]$VMPassword
+    [String]$StorageSuffix
     
 )
 
-# Create the credential for mounting the file share under the local admin context
-$Password = ConvertTo-SecureString -String $VMPassword -AsPlainText -Force
-[pscredential]$Credential = New-Object System.Management.Automation.PSCredential ($VMUsername, $Password)
-
-Invoke-Command -Credential $Credential -ScriptBlock {
-    $ErrorActionPreference = 'Stop'
-    
-    function Write-Log
-    {
-        param(
-            [parameter(Mandatory)]
-            [string]$Message,
-            
-            [parameter(Mandatory)]
-            [string]$Type
-        )
-        $Path = 'C:\cse.txt'
-        if(!(Test-Path -Path $Path))
-        {
-            New-Item -Path 'C:\' -Name 'cse.txt' | Out-Null
-        }
-        $Timestamp = Get-Date -Format 'MM/dd/yyyy HH:mm:ss.ff'
-        $Entry = '[' + $Timestamp + '] [' + $Type + '] ' + $Message
-        $Entry | Out-File -FilePath $Path -Append
-    }
-    try
-    {
-        # Save the password so the drive will persist on reboot
-        cmd.exe /C "cmdkey /add:`"$Using:StorageAccountName.file.$Using:StorageSuffix`" /user:`"localhost\$Using:StorageAccountName`" /pass:`"$Using:StorageKey`""
-        Write-Log -Message "Saved the credential successfully." -Type 'INFO' 
-        
-        # Mount the drive
-        New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$Using:StorageAccountName.file.$Using:StorageSuffix\$Using:ShareName" -Persist
-        Write-Log -Message "Mounted the drive successfully." -Type 'INFO' 
-    }
-    catch {
-        Write-Log -Message $_ -Type 'ERROR'
-    }
-}
+$Share = "\\$StorageAccountName.file.$StorageSuffix\$ShareName"
+$DeleteShare = 'net use z: /delete'
+$AddShare = "net use z: $Share $StorageKey /user:Azure\$StorageAccountName"
+$Path = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup'
+$File = New-Item -Path $Path -Name 'Share.bat' -ItemType 'File' -Force
+$DeleteShare | Out-File -FilePath $File.FullName -Append -Force
+$AddShare | Out-File -FilePath $File.FullName -Append -Force
